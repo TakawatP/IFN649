55f7cbdce1829cadf02246c158b05bda36f5cbb0
TakawatP <takawat.p@gmail.com>
2024-10-20 20:48:50 +1000

    update ip

--- Assignment 2/auto.py
+++ Assignment 2/auto.py
@@ -1,4 +1,4 @@
 import paho.mqtt.publish as publish
 
-publish.single("home/lighting", "AUTO", hostname="3.107.83.64")
+publish.single("home/lighting", "AUTO", hostname="13.55.201.188")
 print("Done")
--- Assignment 2/controller_off.py
+++ Assignment 2/controller_off.py
@@ -1,4 +1,4 @@
 import paho.mqtt.publish as publish
 
-publish.single("home/lighting", "Light OFF", hostname="3.107.83.64")
+publish.single("home/lighting", "Light OFF", hostname="13.55.201.188")
 print("Done")
